6918aa906f4f8085293883c11d56792f0dfe0133
Anna Steckel <annaextrasteckel@gmail.com>
2025-02-02 15:59:55 -0800

    basic app barebones complete; starting on styling

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+// requiring path for styling
+const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -15,16 +19,22 @@ mongoose.connection.on("connected", () => {
 
 const Bird = require('./models/bird.js');
 
-// still just accepting that this line of code is needed and not understanding why/what it does...
+// still just accepting that thes lines of code are needed but still not understanding why...
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
+
+// 'middleware to serve static files from the directory'
+app.use(express.static(path.join(__dirname, "public")));
 
 // ----------------------------ROUTES----------------------------
 
 // INDUCES
 
-// / home page
-app.get('/', (req, res) => {
-    res.render('index.ejs')
+// HOME / LANDING PAGE
+// /
+app.get('/', async (req, res) => {
+    res.render('index.ejs');
 });
 
 // INDEX: list of all birds
@@ -38,20 +48,25 @@ app.get('/birds', async (req, res) => {
 
 // NEW: show a form to create a new bird (how is this different than create?)
 // /birds/new
-app.get('/birds/new', async (req, res) => {
+app.get('/birds/new', (req, res) => {
     res.render('birds/new.ejs');
 });
 
 // DELETE: delete a bird by id
-// /birds/:id
-// but how are we actually accessing this???
-app.delete('/birds/:id', async (req, res) => {
-    const deletedBird = await Bird.findByIdAndDelete(id);
-    console.log('Deleted bird: ', deletedBird);
+// /birds/:birdId
+app.delete('/birds/:birdId', async (req, res) => {
+    await Bird.findByIdAndDelete(req.params.birdId);
+    res.redirect('/birds');
 });
 
 // UPDATE: update a bird by id
-// /birds/:id
+// /birds/:birdId
+// pairs with EDIT route (EDIT collects data and UPDATE processes data)
+app.put('/birds/:birdId', async (req, res) => {
+    await Bird.findByIdAndUpdate(req.params.birdId, req.body);
+    res.redirect(`/birds/${req.params.birdId}`);
+    // res.send('testing update');
+});
 
 // CREATE: create a new bird (how is this different than new?)
 // /birds
@@ -62,13 +77,29 @@ app.post('/birds', async (req, res) => {
 });
 
 // EDIT: show a form to edit an existing bird
-// /birds/:id/edit
+// /birds/:birdId/edit
+// pairs with UPDATE route (EDIT collects data and UPDATE processes data)
+app.get('/birds/:birdId/edit', async (req, res) => {
+    const foundBird = await Bird.findById(req.params.birdId);
+    res.render('birds/edit.ejs', { bird: foundBird } );
+});
 
 // SHOW: display a bird by id
-// /birds/:id
+// /birds/:birdId
+app.get("/birds/:birdId", async (req, res) => {
+    const foundBird = await Bird.findById(req.params.birdId);
+    res.render('birds/show.ejs', { bird: foundBird });
+});
 
 // ----------------------------PORTS----------------------------
 
 app.listen(3000, () => {
     console.log('Listening on port 3000.');
 });
+
+// ----------------------------QUESTIONS----------------------------
+
+
+// ID syntax in edit.ejs and new.ejs files
+
+// lots of bugs to figure out in edit.ejs
